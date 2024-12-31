8f618c2d7007cec852bb16535df71511ac883ec9
Jhon Marcel Adelantar <adelzarajhonmarcel@gmail.com>
2024-12-31 13:08:22 +0800

    fix the size of the font

--- src/components/BalanceSheet.tsx
+++ src/components/BalanceSheet.tsx
@@ -4,6 +4,7 @@ export const BalanceSheet = () => {
       <h2>Your Balance:</h2>
       <div className="Balance_container">
         <p>₱3000000</p>
+        <button type="button" className="btn btn-success">Deposit</button>
       </div>
     </>
   );
--- src/components/History.tsx
+++ src/components/History.tsx
@@ -2,10 +2,10 @@
 export const History = () => {
   return (
     <>
-    <h2 style={{right: "51vh", bottom: "50px"}}>History</h2>
+    <h2 style={{right: "47vh", bottom: "50px"}}>History:</h2>
     <div className="History_container">
       <input type="text" placeholder="Purchased Item"/>
-      <input type="text"placeholder="Purchase Item" />
+      <input type="text" placeholder="Purchased Item"/>
     </div>
     </>
   )
